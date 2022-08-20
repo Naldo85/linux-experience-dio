211715044d44af607383dbd34a4918905965cdf8
Perkles <scleinaldo85@gmail.com>
2022-08-20 15:03:39 -0300

    fist commit linux experience

---
+++ iac1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Carlos123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Maria123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Joao123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Debora123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Sebastiana123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Roberto123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Josefina123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Amanda123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Rogerio123) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios...."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim....."
+
